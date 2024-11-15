35bff2c2239ee7c9ce53a08871fab3611d694943
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2024-11-15 16:04:17 -0500

    Travail Terminé

--- main.py
+++ main.py
@@ -37,20 +37,22 @@ class CreatureStats:
 
 
 class NPC:
-    def __init__(self, name, job, creature_type, race):
+    def __init__(self, name, job, race, creature_type):
         self.name = name
         self.job = job
         self.creature_type = creature_type
         self.race = race
         self.stats = CreatureStats()
-        self.armour_class = 12
+        self.armour_class = randint(8, 12)
+        self.hit_points = randint(10, 20)
 
     def display_info(self):
         print(f"\n"
               f"\nNom: {self.name}"
               f"\nProfession: {self.job}"
-              f"\nType de creature: {self.creature_type}"
               f"\nRace: {self.race}"
+              f"\nType de creature: {self.creature_type}"
+              f"\nPoints de vie: {self.hit_points}"
               f"\n"
               f"\nStats de {self.name}:"
               f"\n\tForce: {self.stats.strength}"
@@ -62,5 +64,71 @@ class NPC:
               f"\n")
 
 
-test = NPC("Kerzin", "Forgeron", "Humanoïde", "Nain")
-test.display_info()
+class Kobold(NPC):
+    def __init__(self, name, job):
+        super().__init__(name, job, "Kobold", "Humanoïde")
+        self.proficiency_bonus = 2
+
+    def attack(self, target, target_ac):
+        d20 = randint(1, 20)
+        dagger_hit = d20 + self.stats.dexterity_modifier + self.proficiency_bonus
+
+        if d20 == 1:
+            return 0
+
+        elif target_ac > dagger_hit:
+            return 0
+
+        elif target_ac <= dagger_hit:
+            dagger_damage = randint(1, 4) + self.stats.dexterity_modifier
+            return dagger_damage
+
+        elif d20 == 20:
+            dagger_damage = randint(1, 4) + randint(1, 4) + self.stats.dexterity_modifier
+            return dagger_damage
+
+    def receive_damage(self, to_hit, damage):
+        if to_hit < self.armour_class:
+            pass
+
+        elif to_hit >= self.armour_class:
+            self.hit_points -= damage
+
+        elif to_hit:
+            self.hit_points -= damage
+
+
+class Hero(NPC):
+    def __init__(self, name, job, race, creature_type):
+        super().__init__(name, job, race, creature_type)
+        self.proficiency_bonus = 2
+        self.shield = 2
+        self.armour_class += self.shield
+
+    def attack(self, target, target_ac):
+        d20 = randint(1, 20)
+        longsword_hit = d20 + self.stats.dexterity_modifier + self.proficiency_bonus
+
+        if d20 == 1:
+            return 0
+
+        elif target_ac > longsword_hit:
+            return 0
+
+        elif target_ac <= longsword_hit:
+            longsword_damage = randint(1, 4) + self.stats.strength_modifier
+            return longsword_damage
+
+        elif d20 == 20:
+            longsword_damage = randint(1, 4) + randint(1, 4) + self.stats.strength_modifier
+            return longsword_damage
+
+    def receive_damage(self, to_hit, damage):
+        if to_hit < self.armour_class:
+            self.hit_points -= 0
+
+        elif to_hit >= self.armour_class:
+            self.hit_points -= damage
+
+        elif to_hit:
+            self.hit_points -= damage
